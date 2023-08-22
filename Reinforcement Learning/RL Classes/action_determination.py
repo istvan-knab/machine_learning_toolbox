c5adc5aab6451b9ef0d98b6910ae668ab5b0ff72
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2023-08-22 20:09:18 +0200

    Update action_determination.py

--- Reinforcement Learning/RL Classes/action_determination.py
+++ Reinforcement Learning/RL Classes/action_determination.py
@@ -1,21 +1,44 @@
 import random as random
+import torch
+from torch import FloatTensor
+
+
 class EpsilonGreedyPolicy:
+
     def __init__(self, EPSILON_DECAY):
-        self.action_set = ("explore","exploit")
+        self.action_set = ("explore", "exploit")
         self.EPSILON_DECAY = EPSILON_DECAY
         self.epsilon = 1
 
     def step(self) -> float:
+        """
+        This function is the connection between the agent and the epsilon greedy
+        policy
+        :return: A choosen value from the action set
+        """
         action_type = self.choose_action_type()
-        action = self.choose_action()
+        action = self.choose_action(action_type)
         self.update()
 
         return action
 
     def update(self):
         self.epsilon = self.EPSILON_DECAY * self.epsilon
-    def choose_action(self):
-        pass
+
+    def choose_action(self, action_type:  str) -> float:
+        """
+        Here will be choosen the random or the optimal solution
+        :param action_type: explore or exploit
+        :return: action in form of a numerical output
+        """
+        if action_type == "explore":
+            pass
+        else:
+            pass
+
+        action = None
+        return action
+
     def choose_action_type(self):
         baseline = random.random()
         if self.epsilon >= baseline:
