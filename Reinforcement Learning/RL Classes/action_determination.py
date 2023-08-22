72c57a52e0d3f58e793a1045d3b8b6a6e290e60e
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2023-08-22 19:48:03 +0200

    epsilon half

--- Reinforcement Learning/RL Classes/action_determination.py
+++ Reinforcement Learning/RL Classes/action_determination.py
@@ -1,12 +1,26 @@
+import random as random
 class EpsilonGreedyPolicy:
-    def __init__(self):
+    def __init__(self, EPSILON_DECAY):
         self.action_set = ("explore","exploit")
+        self.EPSILON_DECAY = EPSILON_DECAY
+        self.epsilon = 1
 
-    def step(self):
-        self.choose_action()
+    def step(self) -> float:
+        action_type = self.choose_action_type()
+        action = self.choose_action()
         self.update()
 
+        return action
+
     def update(self):
-        pass
+        self.epsilon = self.EPSILON_DECAY * self.epsilon
     def choose_action(self):
         pass
+    def choose_action_type(self):
+        baseline = random.random()
+        if self.epsilon >= baseline:
+            action_type = self.action_set[0]
+        else:
+            action_type = self.action_set[1]
+
+        return action_type
