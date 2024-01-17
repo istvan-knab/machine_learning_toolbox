c3bef541e5541e19f6d42597b3ccdfa8a06b8663
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2024-01-17 19:47:44 +0100

    add step

--- reinforcement_learning/Environments/TicTacToe/__init__.py
+++ reinforcement_learning/Environments/TicTacToe/__init__.py
@@ -3,11 +3,30 @@ import numpy as np
 import pygame
 class TicTacToe(gym.Env):
     def __init__(self):
+
         self.state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        self.info = {}
+
     def step(self, action):
-        ...
+
+        self.state = np.reshape(self.state, newshape=(9,1))
+        if self.state[action] == 1:
+            done = True
+            reward = -1
+        else:
+            self.state[action] = 1
+            reward = 0.5
+            done = False
+
+        self.state = np.reshape(self.state, newshape=(3,3))
+
+        return self.state, reward, done, self.info
+
     def reset(self):
-        ...
+
+        self.state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+
+        return self.state
     def render(self, mode='human'):
         ...
     def close(self):
@@ -15,4 +34,6 @@ class TicTacToe(gym.Env):
     def seed(self, seed):
         ...
     def action_handler(self):
+        ...
+    def check_done(self):
         ...
